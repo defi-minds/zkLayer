16e7d3a1760f115979a8ebe49b45df5cba16d829
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-04-02 23:27:57 +0100

    Update verifier.sol

--- zkt/tokens/zkbank/verifier.sol
+++ zkt/tokens/zkbank/verifier.sol
@@ -1,46 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 library Verifier {
-    function verifyDepositProof(uint256 _amount, address _token, uint256 _timestamp, bytes32 _merkleRoot, bytes memory _proof) public pure returns (bool) {
+    function verifyDepositProof(
+        uint256 _amount,
+        address _token,
+        uint256 _timestamp,
+        bytes32 _merkleRoot,
+        bytes memory _proof
+    ) public pure returns (bool) {
         bytes32 leaf = keccak256(abi.encodePacked(_amount, _token, _timestamp));
         return MerkleProof.verify(_proof, _merkleRoot, leaf);
     }
     
     function encrypt(bytes memory _data, address _recipient) public pure returns (bytes memory) {
-        return abi.encode(_data, _recipient);
+        bytes memory encodedData = abi.encodePacked(_data, _recipient);
+        return encodedData;
     }
     
-    function decrypt(bytes memory _encryptedData) public pure returns (bytes memory, address) {
-        (bytes memory data, address recipient) = abi.decode(_encryptedData, (bytes, address));
-        return (data, recipient);
+    function decrypt(bytes memory _encryptedData) public pure returns (bytes memory) {
+        (bytes memory data, ) = abi.decode(_encryptedData, (bytes, address));
+        return data;
     }
     
     function verify(bytes memory _data, bytes memory _signature, address _signer) public pure returns (bool) {
         bytes32 messageHash = keccak256(_data);
-        bytes32 prefixedHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
-        address recoveredAddress = ecrecover(prefixedHash, uint8(_signature[0]), abi.decode(_signature[1:33], uint256), abi.decode(_signature[33:65], uint256));
-        return recoveredAddress == _signer;
+        address recoveredSigner = ECDSA.recover(messageHash, _signature);
+        return recoveredSigner == _signer;
     }
 }
 
 library MerkleProof {
-    function verify(bytes memory proof, bytes32 root, bytes32 leaf) public pure returns (bool) {
+    function verify(
+        bytes memory proof,
+        bytes32 root,
+        bytes32 leaf
+    ) internal pure returns (bool) {
         bytes32 computedHash = leaf;
-        
         for (uint256 i = 0; i < proof.length; i += 32) {
             bytes32 proofElement;
             assembly {
                 proofElement := mload(add(proof, add(32, i)))
             }
-            
             if (computedHash < proofElement) {
                 computedHash = keccak256(abi.encodePacked(computedHash, proofElement));
             } else {
                 computedHash = keccak256(abi.encodePacked(proofElement, computedHash));
             }
         }
-        
         return computedHash == root;
     }
 }
