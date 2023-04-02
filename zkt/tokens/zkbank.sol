c92162b92e6f036fdfb78006df53b370321e7f2d
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-04-03 00:01:04 +0100

    Update zkbank.sol

--- zkt/tokens/zkbank.sol
+++ zkt/tokens/zkbank.sol
@@ -1,75 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./zkbank/verifier.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "./zkbank/Verifier.sol";
+import "./zkbank/MerkleProof.sol";
 
-interface IERC20 {
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function balanceOf(address account) external view returns (uint256);
-    function encryptData(bytes memory _data, address _recipient) external view returns (bytes memory);
-    function decryptData(bytes memory _encryptedData) external view returns (bytes memory);
-    function verifySignature(bytes memory _data, bytes memory _signature, address _signer) external view returns (bool);
+interface IVerifier {
+    function verify(bytes calldata _data, bytes calldata _signature, address _signer) external view returns (bool);
+    function encryptData(bytes calldata _data, address _recipient) external view returns (bytes memory);
+    function verifyDepositProof(uint256 _amount, address _token, uint256 _timestamp, bytes32 _merkleRoot, bytes calldata _merkleProof) external view returns (bool);
 }
 
-contract ConfidentialBank is IConfidentialTransaction {
-    struct Deposit {
-        uint256 amount;
-        address token;
-        uint256 timestamp;
-        bytes32 merkleRoot;
+contract ZkBank {
+    using SafeMath for uint256;
+    mapping(address => uint256) public balances;
+    mapping(bytes32 => bool) public nullifiers;
+    uint256 public totalDeposits;
+
+    event Deposit(address indexed account, uint256 amount, bytes32 indexed nullifier);
+    event Transfer(address indexed token, uint256 amount, address indexed recipient, bytes encryptedData);
+
+    function deposit(bytes32 _nullifier) public payable {
+        require(msg.value > 0, "Deposit amount must be greater than 0");
+        balances[msg.sender] = balances[msg.sender].add(msg.value);
+        nullifiers[_nullifier] = true;
+        totalDeposits = totalDeposits.add(1);
+        emit Deposit(msg.sender, msg.value, _nullifier);
     }
-    
-    mapping(address => Deposit[]) public deposits;
-    mapping(bytes32 => bool) public usedNonces;
-    address public owner;
-    address public verifier;
-    
-    event DepositEvent(address indexed account, uint256 amount, address token, uint256 timestamp, bytes32 merkleRoot);
-    event WithdrawEvent(address indexed account, uint256 amount, address token);
-    
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can call this function");
-        _;
-    }
-    
-    constructor(address _verifier) {
-        owner = msg.sender;
-        verifier = _verifier;
-    }
-    
-    function encryptData(bytes memory _data, address _recipient) public view override returns (bytes memory) {
-        return Verifier(verifier).encrypt(_data, _recipient);
-    }
-    
-    function decryptData(bytes memory _encryptedData) public view override returns (bytes memory) {
-        return Verifier(verifier).decrypt(_encryptedData);
-    }
-    
-    function verifySignature(bytes memory _data, bytes memory _signature, address _signer) public view override returns (bool) {
-        return Verifier(verifier).verify(_data, _signature, _signer);
-    }
-    
-    function deposit(uint256 _amount, address _token, uint256 _timestamp, bytes32 _merkleRoot, bytes memory _proof) public {
-        require(IERC20(_token).transferFrom(msg.sender, address(this), _amount), "Transfer failed");
-        require(!usedNonces[_merkleRoot], "Merkle root already used");
-        require(Verifier(verifier).verifyDepositProof(_amount, _token, _timestamp, _merkleRoot, _proof), "Invalid proof");
-        usedNonces[_merkleRoot] = true;
-        deposits[msg.sender].push(Deposit(_amount, _token, _timestamp, _merkleRoot));
-        emit DepositEvent(msg.sender, _amount, _token, _timestamp, _merkleRoot);
-    }
-    
-    function withdraw(uint256 _index, bytes memory _encryptedData) public {
-        Deposit memory depositItem = deposits[msg.sender][_index];
-        require(IERC20(depositItem.token).balanceOf(address(this)) >= depositItem.amount, "Not enough balance");
-        require(!usedNonces[depositItem.merkleRoot], "Merkle root already used");
-        bytes memory decryptedData = decryptData(_encryptedData);
-        require(verifySignature(decryptedData, abi.encode(msg.sender, depositItem.amount, depositItem.token), msg.sender), "Invalid signature");
-        usedNonces[depositItem.merkleRoot] = true;
-        require(IERC20(depositItem.token).transfer(msg.sender, depositItem.amount), "Transfer failed");
-        emit WithdrawEvent(msg.sender, depositItem.amount, depositItem.token);
-    }
-    
-    function setVerifier(address _verifier) public onlyOwner {
-        verifier = _verifier;
+
+    function transfer(address _token, uint256 _amount, address _recipient, bytes memory _data, bytes memory _signature, bytes32 _merkleRoot, bytes memory _proof, address verifier) public {
+        require(IVerifier(verifier).verify(_data, _signature, msg.sender), "Invalid signature");
+        bytes memory encryptedData = IVerifier(verifier).encryptData(_data, _recipient);
+        require(IVerifier(verifier).verifyDepositProof(_amount, _token, block.timestamp, _merkleRoot, _proof), "Invalid proof");
+        require(IERC20(_token).transferFrom(msg.sender, _recipient, _amount), "Transfer failed");
+        emit Transfer(_token, _amount, _recipient, encryptedData);
     }
 }
